f59ee3e9c7a7fb6868812afa672a8b102e7d1abb
Ankit Tanwar <tankitofficial01@gmail.com>
2023-09-29 21:30:13 +0530

    new

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -2,6 +2,7 @@
 import Header from '../../components/layouts/Header';
 import Aside from '../../components/layouts/Aside';
 import Footer from '../../components/layouts/Footer';
+import Script from 'next/script';
 
 
 export default function DashboardLayout({ children }) {
@@ -15,16 +16,16 @@ export default function DashboardLayout({ children }) {
 
 
             <Script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" data-nscript="afterInteractive"></Script>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.42.0/apexcharts.min.js" data-nscript="afterInteractive"></Script>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.js" data-nscript="afterInteractive"></Script>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.3/echarts.min.js" data-nscript="afterInteractive"></Script>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.min.js" data-nscript="afterInteractive"></Script>
-        <Script src="/assets/vendor/simple-datatables/simple-datatables.js" data-nscript="afterInteractive"></Script>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.7.0/tinymce.min.js" data-nscript="afterInteractive"></Script>
-        <Script src="/assets/vendor/php-email-form/validate.js" data-nscript="afterInteractive"></Script>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.42.0/apexcharts.min.js" data-nscript="afterInteractive"></Script>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.js" data-nscript="afterInteractive"></Script>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.3/echarts.min.js" data-nscript="afterInteractive"></Script>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.min.js" data-nscript="afterInteractive"></Script>
+            <Script src="/assets/vendor/simple-datatables/simple-datatables.js" data-nscript="afterInteractive"></Script>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.7.0/tinymce.min.js" data-nscript="afterInteractive"></Script>
+            <Script src="/assets/vendor/php-email-form/validate.js" data-nscript="afterInteractive"></Script>
 
-  
-        <Script src="/assets/js/main.js" data-nscript="afterInteractive"></Script>
+
+            <Script src="/assets/js/main.js" data-nscript="afterInteractive"></Script>
 
         </>
     )
